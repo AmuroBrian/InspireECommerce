d5de515d14ac96d5cf58303bd4cd918cd457a415
Brian Perez <perezbrian.sti@gmail.com>
2025-03-05 11:48:25 +0800

    About Page

--- src/app/(landingpage)/about/page.js
+++ src/app/(landingpage)/about/page.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { AboutInspire } from '../components/AboutInspire';
+import AboutEcommerce from '../components/AboutEcommerce';
 
 
 export default function About() {
     return (
         <div>
-           
+            <AboutInspire />
+            <AboutEcommerce />
         </div>
     )
 }
