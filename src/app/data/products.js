7b245d10bae7a3c149e19cdb143c9b548526e249
denseeen <villanueva.194566@cubao.sti.edu.ph>
2025-03-13 22:19:45 +0800

    transact

--- src/app/data/products.js
+++ src/app/data/products.js
@@ -183,7 +183,7 @@ const products = [
         info: "Groundbreaking skincare innovation further enhances and delivers deep hydration, nourishment, and protection for radiant, healthy-looking skin.",
         ingredients: "Water, Mineral Oil, Glycerin, BG, Wasabi Seed Oil, Behenyl Alcohol,Patyl Alcohol, Stearic Acid, Beeswax, Tri (Caprylic Acid/Capric Acid) Glyceril, Dimethicone, Cetyl Palmitate, Glyceryl Caprylate, Ethyheylglycerin, Phenoxyethanol, Fucoxanthin, Okinawa Mozuku Extract, Umbilical Cord Extract, Hydrolyzed Equine Amniotic Membrane Extract, Sodium Hyaluronate, Hydrolyzed Alkyl (C12-13) Glyceryl Hyaluronate, Hydrolyzed Hyaluronic Acid, Methyl Carboxymethyl Phenyl Amino Carboxy Propylphosphonate, Trisodium Ascorbyl Palmitate Phosphate, Centella Asiatica Extract, Licorice Root Extract, Rosemary Leaf Extract, Chamomile Flower Extract, Scutellaria Root Extract, Japanese Knotweed Root Extract, Peanut Seed Coat Extract, Propolis Extract, Pentylene Glycol, Ethanol, Lemongrass Oil, Lavender Oil, Xanthan Gum, Lecithin, Carbomer, Arginine, Tocopherol.",
         usage: "lorem ipsum",
-        imgsrc: "/images/fomlotion.png",
+        imgsrc: "/images/fomlotion2.png",
         imageGallery: [
             "/images/YBYMorningMask(7pcs).png",  // Default image
             "/images/YBYMorningMask(30pcs).png",  // Side view
